70723671fa43d77c8d87b74da0edaffbd134a458
Smit Vekaria <smitvekaria56@gmail.com>
2024-08-10 01:14:16 -0400

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,32 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-    return (
-        <div>Home</div>
-    );
-}
+	const loggedIn = { firstName: "Smit", lastName: "Vekaria", email: 'contact@smit.com' };
+	return (
+		<section className="home">
+			<div className="home-content">
+				<header className="home-header">
+					<HeaderBox
+						type="greeting"
+						title="Welcome"
+						user={loggedIn?.firstName || "Guest"}
+						subtext="Access and manage your account and transactions efficiently."
+					/>
 
-export default Home
+					<TotalBalanceBox
+						accounts={[]}
+						totalBanks={1}
+						totalCurrentBalance={1250.35}
+					/>
+				</header>
+			</div>
+
+			<RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50}, {currentBalance: 500.20}]} />
+		</section>
+	);
+};
+
+export default Home;
